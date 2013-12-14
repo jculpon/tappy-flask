011e037add906847fa43ea31cf34084c4e0a082b
Jamie Culpon <jculpon@gmail.com>
2013-12-13 18:14:27 -0800

    Port Location over
    Extracts the Location class from the old Tappy Terror and brings it over to
    Flask-land. This was straightforward: the class is self contained and just
    encapsulates a bit of logic about mob spawning.
    
    The only hard decision was what to do about the things that were set in module
    level variables as configuration in the original Tappy Terror. I've decided to
    keep them as module level variables for now just for the ease of porting, but
    I think they should go into their own config structure if we end up with
    a bunch of them. We didn't change these much when we were setting up the
    game initially, but I still find it icky that we had to tune them by
    changing code, not data.
    
    Adds tests for mob spawning and removal to ensure that gameplay asusmptions
    about mob counts aren't violated.

--- tappy.py
+++ tappy.py
@@ -8,6 +8,7 @@
     :license: MIT; details in LICENSE
 """
 
+# Flask setup
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -26,6 +27,64 @@ def game_index():
     """Render the current state of the game for the web"""
     return render_template('game.html')
 
+# End Flask setup
+
+# Game config
+# These constants are the variables that control all the
+# game logic -  how often the board resets, mobs spawn and how many points
+# you get for killing mobs and holding territory.
+# For now they are hard coded in but I would like them to be read in from a file
+# where we can change them
+# Maximum number of mobs that can be spawned in a given location
+max_mobs_in_loc = 25
+
+# Number of mobs spawned in a location by default
+initial_mob_count = 1
+
+
+# Game logic    
+class Location(object):
+    """Handles all information for locations in Tappy Terror
+
+    Currently just a holder for team and mob count info
+    """
+
+    def __init__(self, team=None, mob_count=initial_mob_count):
+        """Make a location.
+        
+        Arguments:
+        - `team`: The team that currently owns this Location
+        - `mob_count`: Number of mobs in the Location
+        """
+        self.team = team
+        self.mob_count = mob_count
+    
+    def spawn_mob(self, count=1):
+        """Spawn a mob in this location
+        
+        Arguments:
+        - `count`: Number of mobs to spawn. defaults to 1
+        """
+        updated_count = self.mob_count + count
+        if updated_count > max_mobs_in_loc:
+            updated_count = max_mobs_in_loc
+        self.mob_count = updated_count
+
+    def remove_mob(self, count=1):
+        """Remove a mob from this location
+        
+        Arguments:
+        - `count`: Number of mobs to spawn. defaults to 1
+        """
+        updated_count = self.mob_count - count
+        if updated_count < 0:
+            updated_count = 0
+        self.mob_count = updated_count
+
+    def has_mobs(self):
+        """Returns true if and only if the location has mobs in it
+        """
+        return self.mob_count > 0
 
 if __name__ == '__main__':
     app.run()
--- tappy_tests.py
+++ tappy_tests.py
@@ -20,5 +20,26 @@ class TappyTerrorWebTestCase(unittest.TestCase):
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
 
+class LocationTestCase(unittest.TestCase):
+    def test_mob_spawn(self):
+        loc = tappy.Location('some team', 0)
+        loc.spawn_mob()
+        self.assertTrue(loc.has_mobs())
+        self.assertGreater(loc.mob_count, 0)
+
+        # Spawning more than max mobs should only ever result in max mobs
+        loc.spawn_mob(tappy.max_mobs_in_loc + 1)
+        self.assertTrue(loc.has_mobs())
+        self.assertEqual(loc.mob_count, tappy.max_mobs_in_loc)
+
+    def test_remove_mob(self):
+        loc = tappy.Location('some team', tappy.max_mobs_in_loc)
+        self.assertTrue(loc.has_mobs())
+        loc.remove_mob(1)
+        self.assertEquals(loc.mob_count, tappy.max_mobs_in_loc - 1)
+        # mob count should never be negative
+        loc.remove_mob(tappy.max_mobs_in_loc)
+        self.assertEquals(loc.mob_count, 0)
+
 if __name__ == '__main__':
     unittest.main()
